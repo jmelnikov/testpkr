16791ccff62dd8ec88d832205c1c2fe9910ad969
Jury <melnikov.j@gmail.com>
2020-11-23 18:38:14 +0300

    Initialization

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=testpkr',
+    'username' => 'testpkr',
+    'password' => 'testpkr',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
